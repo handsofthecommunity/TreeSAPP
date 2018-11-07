8cd71994fa8fa026de36cb9185d854fc6fd6ff5a
Grace Zhang <gracezhang72@gmail.com>
2018-11-07 13:40:53 -0800

    fix indents, travis yaml

--- tests/file_parsers_test.py
+++ tests/file_parsers_test.py
@@ -21,7 +21,6 @@ def create_parser(treesapp,targets, reftree):
 class ParserTest(unittest.TestCase):
 
     def check_parse_ref_build_params_out(self, expected_vals, targets, marker_build_dict):
-
         for i in range(0, len(targets)):
                 assert (marker_build_dict[targets[i]].cog == expected_vals[i][0])
                 assert (marker_build_dict[targets[i]].denominator == expected_vals[i][1])
@@ -35,9 +34,7 @@ class ParserTest(unittest.TestCase):
 
 
     def test_parse_ref_build_params(self):
-            expected_out = [['McrA', 'M0701', 'prot', 'PROTGAMMALG', '0.97', 'Classes', '01_Aug_2018', '', ''],
-                            ['hzs', 'A0100', 'prot', 'PROTGAMMAJTTDCMUT', '0.99', 'Species', '27_Apr_2018', '', ''],
-                            ['narG', 'D0101', 'prot', 'PROTGAMMALG', '80', 'Genera', '23_Jan_2018', '', '']]
+        expected_out = [['McrA', 'M0701', 'prot', 'PROTGAMMALG', '0.97', 'Classes', '01_Aug_2018', '', ''], ['hzs', 'A0100', 'prot', 'PROTGAMMAJTTDCMUT', '0.99', 'Species', '27_Apr_2018', '', ''], ['narG', 'D0101', 'prot', 'PROTGAMMALG', '80', 'Genera', '23_Jan_2018', '', '']]
 
         args = create_parser('/home/travis/build/hallamlab/TreeSAPP/', 'M0701', 'p')
         marker_build_dict = file_parsers.parse_ref_build_params(args)
@@ -50,7 +47,7 @@ class ParserTest(unittest.TestCase):
 
 
     def test_exit(self):
-            args = create_parser('~/', 'ALL', 'p')
+        args = create_parser('~/', 'ALL', 'p')
         with pytest.raises(SystemExit) as pytest_wrapped_e:
                 file_parsers.parse_ref_build_params(args)
                 assert pytest_wrapped_e.type == SystemExit
@@ -58,9 +55,7 @@ class ParserTest(unittest.TestCase):
                 
 
     def test_parse_cog_list(self):
-            expected_out = [['McrA', 'M0701', 'prot', 'PROTGAMMALG', '0.97', 'Classes', '01_Aug_2018', 'Methyl coenzyme M reductase alpha subunit', 'functional_cogs'],
-                            ['hzs', 'A0100', 'prot', 'PROTGAMMAJTTDCMUT', '0.99', 'Species', '27_Apr_2018', 'Hydrazine synthase (hzs)', 'functional_cogs'],
-                            ['narG', 'D0101', 'prot', 'PROTGAMMALG', '80', 'Genera', '23_Jan_2018', 'nitrate reductase / nitrite oxidoreductase, alpha subunit', 'functional_cogs']]
+        expected_out = [['McrA', 'M0701', 'prot', 'PROTGAMMALG', '0.97', 'Classes', '01_Aug_2018', 'Methyl coenzyme M reductase alpha subunit', 'functional_cogs'], ['hzs', 'A0100', 'prot', 'PROTGAMMAJTTDCMUT', '0.99', 'Species', '27_Apr_2018', 'Hydrazine synthase (hzs)', 'functional_cogs'], ['narG', 'D0101', 'prot', 'PROTGAMMALG', '80', 'Genera', '23_Jan_2018', 'nitrate reductase / nitrite oxidoreductase, alpha subunit', 'functional_cogs']]
 
         args = create_parser('/home/travis/build/hallamlab/TreeSAPP/', 'M0701', 'p')
         marker_build_dict = file_parsers.parse_cog_list(args, file_parsers.parse_ref_build_params(args))
@@ -73,12 +68,12 @@ class ParserTest(unittest.TestCase):
 
 
     def test_read_species_translation_files(self):
-            args = create_parser('/home/travis/build/hallamlab/TreeSAPP/', 'M0701', 'p')
-            marker_build_dict = file_parsers.parse_ref_build_params(args)
-            marker_build_dict = file_parsers.parse_cog_list(args, marker_build_dict)
-            tree_numbers_translation = file_parsers.read_species_translation_files(args, marker_build_dict)
-            assert(tree_numbers_translation['M0701'][0].lineage == 'cellular organisms; Archaea')
-            assert tree_numbers_translation['M0701'][0].complete
-            assert(tree_numbers_translation['M0701'][0].number == '1')
-            assert(tree_numbers_translation['M0701'][0].description == "NM4 | NM423249334157")
+        args = create_parser('/home/travis/build/hallamlab/TreeSAPP/', 'M0701', 'p')
+        marker_build_dict = file_parsers.parse_ref_build_params(args)
+        marker_build_dict = file_parsers.parse_cog_list(args, marker_build_dict)
+        tree_numbers_translation = file_parsers.read_species_translation_files(args, marker_build_dict)
+        assert(tree_numbers_translation['M0701'][0].lineage == 'cellular organisms; Archaea')
+        assert tree_numbers_translation['M0701'][0].complete
+        assert(tree_numbers_translation['M0701'][0].number == '1')
+        assert(tree_numbers_translation['M0701'][0].description == "NM4 | NM423249334157")
 
