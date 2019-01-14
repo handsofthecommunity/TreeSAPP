86a9300666d120c28e2c5f8998043d841204c63f
Grace Zhang <gracezhang72@gmail.com>
2019-01-14 15:05:49 -0800

    test treesapp and fasta tests

--- tests/classy_test.py
+++ tests/classy_test.py
@@ -23,35 +23,35 @@ def init_itol():
     itol.create_jplace_node_map()
     return itol
 
-class TreeSappTest(unittest.TestCase):
+def create_parser(treesapp, targets, reftree):
+    args = argparse.Namespace()
+    args.alignment_mode = 'd'
+    args.reftree = reftree
+    args.targets = [targets]
+    args.treesapp = treesapp
+    args.check_trees = False
+    args.fasta_input = 'test_data/marker_test_suite.faa'
+    args.output = '/home/travis/build/hallamlab/marker_test/'
+    args.output_dir_var = '/home/travis/build/hallamlab/marker_test/various_outputs'
+    args.skip = 'n'
+    args.molecule = 'prot'
+    args.executables = {'BMGE.jar': '/home/travis/build/hallamlab/TreeSAPP/sub_binaries/BMGE.jar', 'hmmalign': '/usr/bin/hmmalign', 'usearch': HOME_DIR + 'sub_binaries/usearch', 'hmmsearch': '/usr/bin/hmmsearch', 'trimal': '/usr/bin/trimal', 'raxmlHPC': '/usr/bin/raxmlHPC', 'hmmbuild': '/usr/bin/hmmbuild', 'prodigal': '/usr/local/bin/prodigal', 'papara': '/usr/bin/papara'}
+    args.reference_data_prefix=''
+    args.num_threads = 3
+    args.output_dir_final = '/home/travis/build/hallamlab/marker_test/final_outputs/'
+    args.formatted_input_file = ''
+    args.composition = 'meta'
+    args.overwrite = True
+    args.delete = False
+    args.reclassify = False
+    args.min_e = 0.0001
+    args.min_acc = 0.7
+    args.min_likelihood = 0.2
+    args.min_seq_length = 30
+    args.perc_aligned = 15
+    return args
 
-    def create_parser(treesapp, targets, reftree):
-        args = argparse.Namespace()
-        args.alignment_mode = 'd'
-        args.reftree = reftree
-        args.targets = [targets]
-        args.treesapp = treesapp
-        args.check_trees = False
-        args.fasta_input = 'test_data/marker_test_suite.faa'
-        args.output = '/home/travis/build/hallamlab/marker_test/'
-        args.output_dir_var = '/home/travis/build/hallamlab/marker_test/various_outputs'
-        args.skip = 'n'
-        args.molecule = 'prot'
-        args.executables = {'BMGE.jar': '/home/travis/build/hallamlab/TreeSAPP/sub_binaries/BMGE.jar', 'hmmalign': '/usr/bin/hmmalign', 'usearch': HOME_DIR + 'sub_binaries/usearch', 'hmmsearch': '/usr/bin/hmmsearch', 'trimal': '/usr/bin/trimal', 'raxmlHPC': '/usr/bin/raxmlHPC', 'hmmbuild': '/usr/bin/hmmbuild', 'prodigal': '/usr/local/bin/prodigal', 'papara': '/usr/bin/papara'}
-        args.reference_data_prefix=''
-        args.num_threads = 3
-        args.output_dir_final = '/home/travis/build/hallamlab/marker_test/final_outputs/'
-        args.formatted_input_file = ''
-        args.composition = 'meta'
-        args.overwrite = True
-        args.delete = False
-        args.reclassify = False
-        args.min_e = 0.0001
-        args.min_acc = 0.7
-        args.min_likelihood = 0.2
-        args.min_seq_length = 30
-        args.perc_aligned = 15
-        return args
+class TreeSappTest(unittest.TestCase):
 
     def create_short_qc_ma_dict():
         qc_ma_dict = {'M0701': {'tests/test_data/expected_phy_file': {'186': '-------------------------------------------------------------------------------------------------------------------------------------------------------------------------KHAGVIQMADILPARRARGPNEPGGIKFGHFGDMIQADPVKATLEVVGAGAMLFDQIWLGSYMSGGYATAAYTDNILDDYCYYGLDYTQEVLNDIATEVTLYGMEQYEQYPTTLESHFGGSQRASVLAAASGISCSLATANSNAGLNGWYMSMLAHKEGWSRLGFFGYDLQDQCGSTNSMSIRPDEGCIGELRGPNYPNYAMNVGHQGEYAAIASAAHYGRQDAWVLSPLIKIAFADPSLKFDFSEPRREFARGAIREFMPAGERSLIIP', '70': 'DDLHFVNNAAIQQMVDDIKRTVIVGMDTAHAVLEKRLGVEVTPETINEYMEAINHALPGGAVVQEHMVEVHPGLVEDCYAKIFTGDDNLADELDKRILIDINKEFPEEQLKSYIGNRTYQVNRVPTIVVRTCDGGTVSRWSAMQIGMSFISAYKLCAGEAAIADFSYAAKHADVIEMGTIMPARRARGPNEPGGVAFGTFADIVQTDPANVSLEVIAGAAALYDQVWLGSYMSGGYATAAYTDDILDDFVYYGMEYTMDVVRDISTEVTLYSLEQYEEYPTLLEDHFGGSQRAAVAAAAAGCSTAFATGNSNAGINGWYLSQILHKEAHSRLGFYGYDLQDQCGASNSLSIRSDEGLIHELRGPNYPNYAMNVGHQPEYAGIAQAPHAARGDAFCTNPLIKVAFADKDLAFDFTSPRKSIAAGALREFMPEGERDLIIP', '59': 'DDLHYVNNAAIQQAWDDIRRTVIVGLNTAHNVLEKRLGIEVTPETITHYLETVNHAMPGAAVVQEHMVETDPLIVQDSYVKVFTGDDELADEIDSAFVLDINKEFPEEALKAEVGGAIWQAVRIPSIVGRVCDGGNTSRWSAMQIGMSMISAYNQCAGEGATGDFAYASKHAEVIHMGTYLPVRRARAENELGGVPFGFMADICQGDPVRVSLEVVALGAALYDQIWLGSYMSGGYATAAYTDNVLDDFTYYGKDYNMDTVLDVGTEVAFYALEQYEEYPALLETHFGGSQRASVVSAAAGCSTAFATGNAQTGLSAWYLAMYLHKEQHSRLGFYGFDLQDQCGAANVFSIRNDEGLPLEMRGPNYPNYAMNVGHQGEYAGIAQAPHAARGDAWAFNPLVKIAFADKNLCFDFSKVREEFAKGALREFEPAGERTAITP'}}}
