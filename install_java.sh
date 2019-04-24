cdcea85abf9698109792ba1e4a197b073fa7b8c6
gracez72 <gracezhang72@gmail.com>
2019-04-24 19:54:40 +0000

    update java installer

--- install_java.sh
+++ install_java.sh
@@ -1,33 +1,21 @@
 #!/bin/bash
 
+mkdir ~/Downloads/
+cd ~/Downloads/
+wget http://storage.googleapis.com/travis-java/jdk-8u211-linux-i586.tar.gz
+mkdir /usr/lib/jvm
+cd /usr/lib/jvm
+tar -xvzf ~/Downloads/jdk-8u211-linux-i586.tar.gz
 
-# if sudo apt-get install oracle-java8-installer -y ; then
-#     echo "Java8 installed."
-# else 
-    url='http://javadl-esd-secure.oracle.com/update/baseline.version'
-    version=$(curl -L $url | grep 1.8)
-		echo $version
-		
-    j_dir="jdk$version"
-    v_num=$(echo $version | cut -d "_" -f 2)
-    download_link=$(curl -i https://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html | grep jdk-8u$v_num-linux-x64.tar.gz | cut -d '"' -f12 | cut -d '/' -f1-8)
+echo "PATH=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/jvm/jdk1.8.0_211/bin:/usr/lib/jvm/jdk1.8.0_211/db/bin:/usr/lib/jvm/jdk1.8.0_211/jre/bin\"
+J2SDKDIR=\"/usr/lib/jvm/jdk1.8.0_211\"
+J2REDIR=\"/usr/lib/jvm/jdk1.8.0_211/jre*\"
+JAVA_HOME=\"/usr/lib/jvm/jdk1.8.0_211\"
+DERBY_HOME=\"/usr/lib/jvm/jdk1.8.0_211/db\"" > /etc/environment
 
-    link=$(curl -i https://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html | grep -o https://download.oracle.com/.*/jdk-8u$v_num-linux-x64.tar.gz)
-    link=${link/https/http}
+source /etc/environment
 
-    cd /var/cache/oracle-jdk8-installer
-    checksum=$(sha256sum jdk-8u201-linux-x64.tar.gz | cut -d ' ' -f 1)
-
-    sed_string='s|JAVA_VERSION=8u.*|JAVA_VERSION=8u'$v_num'|'
-    cd /var/lib/dpkg/info && sudo sed -i $sed_string oracle-java8-installer.*
-
-    sed_string='s|PARTNER_URL=http://download.oracle.com.*|PARTNER_URL='$download_link'/|'
-    echo $sed_string
-    sudo sed -i $sed_string oracle-java8-installer.*
-
-    sed_string='s|SHA256SUM_TGZ=.*|SHA256SUM_TGZ='\"$checksum\"'|'
-    sudo sed -i $sed_string oracle-java8-installer.*
-
-    sed_string='s|J_DIR=jdk.*|J_DIR='$j_dir'|'
-    sudo sed -i $sed_string oracle-java8-installer.* 
-#fi
+sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.8.0_211/bin/java" 0
+sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.8.0_211/bin/javac" 0
+sudo update-alternatives --set java /usr/lib/jvm/jdk1.8.0_211/bin/java
+sudo update-alternatives --set javac /usr/lib/jvm/jdk1.8.0_211/bin/javac
